f12e4a118ff179434d5403e6b4f588a256d1472d
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2026-06-11 20:09:39 +0300

    9. Better errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,35 @@
 const router = require("express").Router()
 const { User } = require("../models")
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === "SequelizeValidationError") {
+    if (
+      error.message ===
+      "Validation error: Validation isEmail on username failed"
+    ) {
+      const message = "Username must be an email address"
+      return res.status(400).json(message)
+    }
+    return res.status(400).json({ error: error.message })
+  }
+  return res.status(500).json({ error: error.message })
+
+  next(error)
+}
+
 router.get("/", async (req, res) => {
   const users = await User.findAll()
   res.json(users)
 })
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     const user = await User.create(req.body)
     res.json(user)
   } catch (error) {
-    return res.status(400).json({ error })
+    next(error)
   }
 })
 
@@ -21,5 +39,7 @@ router.put("/:username", async (req, res) => {
   await user.save()
   res.json(user)
 })
+
+router.use(errorHandler)
 
 module.exports = router
