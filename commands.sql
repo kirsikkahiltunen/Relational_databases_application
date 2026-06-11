54c73d0aca6b5390cb3c4e96437c83136a4f8ccd
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2026-06-11 13:30:54 +0300

    SQL commands for inserting blogs added (2. Console connection)

--- commands.sql
+++ commands.sql
@@ -1,1 +1,3 @@
 CREATE TABLE blogs (id SERIAL PRIMARY KEY, author text, url text NOT NULL, title text NOT NULL, likes INT DEFAULT 0);
+insert into blogs (author, url, title) values ('Testi', 'testiblogi.fi', 'Testiblogi');
+insert into blogs (author, url, title, likes) values ('Testi2', 'koodarinmietteitä.fi', 'Koodarin mietteitä', 3);
