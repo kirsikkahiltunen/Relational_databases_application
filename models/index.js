7f677de869262378fa32c865a350be091b04900c
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2026-06-13 12:52:29 +0300

    20. Reading list

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const { sequelize } = require("../util/db")
 const Blog = require("./blog")
 const User = require("./user")
+const ReadingList = require("./reading_list")
 User.hasMany(Blog)
 Blog.belongsTo(User)
-module.exports = { Blog, User }
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
+module.exports = { Blog, User, ReadingList }
